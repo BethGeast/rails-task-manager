4b0bb7b647b087c2be831d8b4aaea4113800bcd4
beth geast <beth.geast@gmail.com>
2024-03-05 12:11:03 +0000

    read one/all and create get/post complete and working as intended

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-
+  get 'tasks', to: 'tasks#index' # read all
+  get 'tasks/new', to: 'tasks#new' # create
+  post 'tasks', to: 'tasks#create' # create/post
+  get 'tasks/:id', to: 'tasks#show', as: :task # read one
 end
